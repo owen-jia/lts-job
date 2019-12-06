3e041afd7dbb6b23243b27975d46127fc3118c55
贾文军 <jiawenjun@isesol.com>
2019-12-06 14:03:13 +0800

    Job Push failed日志添加节点信息

--- lts-jobtracker/src/main/java/com/github/ltsopensource/jobtracker/support/JobPusher.java
+++ lts-jobtracker/src/main/java/com/github/ltsopensource/jobtracker/support/JobPusher.java
@@ -183,7 +183,7 @@ public class JobPusher {
                             try {
                                 RemotingCommand responseCommand = responseFuture.getResponseCommand();
                                 if (responseCommand == null) {
-                                    LOGGER.warn("Job push failed! response command is null!");
+                                    LOGGER.warn("Job push failed! response command is null!" + nodeGroup + ", identity=" + identity + ", jobList=" + JSON.toJSONString(jobPos));
                                     return;
                                 }
                                 if (responseCommand.getCode() == JobProtos.ResponseCode.JOB_PUSH_SUCCESS.code()) {
