8189cd3c3035ac27450e44bf660d08874a6ecc43
贾文军 <jiawenjun@isesol.com>
2019-12-20 16:08:21 +0800

    Job Push failed日志添加节点信息

--- lts-core/src/main/java/com/github/ltsopensource/core/protocol/command/JobPushRequest.java
+++ lts-core/src/main/java/com/github/ltsopensource/core/protocol/command/JobPushRequest.java
@@ -1,7 +1,6 @@
 package com.github.ltsopensource.core.protocol.command;
 
 import com.github.ltsopensource.core.domain.JobMeta;
-import com.github.ltsopensource.remoting.annotation.NotNull;
 
 import java.util.List;
 
@@ -12,7 +11,6 @@ public class JobPushRequest extends AbstractRemotingCommandBody {
 
 	private static final long serialVersionUID = 2986743693237022215L;
 	
-	@NotNull
     private JobMeta jobMeta;
 
 	//兼容1.7.0 server, at 2019-11-21
