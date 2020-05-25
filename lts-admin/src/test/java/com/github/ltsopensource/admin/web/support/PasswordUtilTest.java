68444084aa33cb660e2e36f26b1e05f063d5abe1
贾文军 <jiawenjun@isesol.com>
2020-05-25 11:30:52 +0800

    删除对javax的base64包依赖测试

--- lts-admin/src/test/java/com/github/ltsopensource/admin/web/support/PasswordUtilTest.java
+++ lts-admin/src/test/java/com/github/ltsopensource/admin/web/support/PasswordUtilTest.java
@@ -1,6 +1,5 @@
 package com.github.ltsopensource.admin.web.support;
 
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 import org.junit.Test;
 
 /**
@@ -20,16 +19,4 @@ public class PasswordUtilTest {
         System.out.println(PasswordUtil.conformPassword("12",enPw));
     }
 
-    @Test
-    public void test01(){
-        String username = "admin",password = "admin123";
-        String result01 = Base64.encode((username+":"+password).getBytes());
-        System.out.println(result01);
-
-        String result02 = new String(com.github.ltsopensource.core.commons.utils.Base64.decodeFast(result01));
-        System.out.println(result02);
-
-        String result03 = new String(java.util.Base64.getEncoder().encode((username+":"+password).getBytes()));
-        System.out.println(result03);
-    }
 }
