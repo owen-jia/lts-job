192de2f773936a1c2300bb70ce6e801c1117f42e
贾文军 <jiawenjun@isesol.com>
2020-06-01 17:19:29 +0800

    test mail send is success

--- lts-core/src/test/java/com/github/ltsopensource/alarm/email/SMTPMailManagerImplTest.java
+++ lts-core/src/test/java/com/github/ltsopensource/alarm/email/SMTPMailManagerImplTest.java
@@ -12,7 +12,7 @@ public class SMTPMailManagerImplTest {
         String host = "smtp.qq.com";
         // 授权码从这里获取 http://service.mail.qq.com/cgi-bin/help?subtype=1&&id=28&&no=1001256
         // lts12345
-        MailManager mailManager = new SMTPMailManagerImpl(host, "2179816070@qq.com", "这里是授权码", "LTS通知中心(notice@lts.com)", true);
+        MailManager mailManager = new SMTPMailManagerImpl(host, "owen-jia@outlook.com", "这里是授权码", "LTS通知中心(notice@lts.com)", true);
         mailManager.send("254963746@qq.com", "测试", "fdsafhakdsjfladslfj呵呵呵");
     }
 }
